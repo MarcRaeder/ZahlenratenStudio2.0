8339209b076984906bd8fdd05a6afb13108fb5f5
Marc Räder <marc.raeder@5minds.de>
2023-01-26 14:42:19 +0100

    create Project

---
+++ Zahlenraten.Intermediate/apps/Zahlenraten.Intermediate/Worker/Example/ExamplePayload.cs
@@ -0,0 +1,11 @@
+namespace Zahlenraten.Intermediate.Worker.Example;
+
+public class ExamplePayload
+{
+    public ExamplePayload(string examplePayloadInput)
+    {
+        this.ExamplePayloadInput = examplePayloadInput;
+    }
+
+    public string ExamplePayloadInput {get; set;}
+}
